d73c3036ba5fb86fa063b8675514f94488fa2bce
Ankit Sharma <ankitsharma2615@gmail.com>
2024-01-04 14:51:33 +0530

    Initial commit

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+    </>
+  );
+};
